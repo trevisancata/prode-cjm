5463895148d4e1d62f6833182ac2728786689a04
trevisancata <trevisancata@gmail.com>
2026-06-18 22:19:46 -0300

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 // ─────────────────────────────────────────────────────────
 // Reemplazá con tus valores de Supabase → Settings → API
 // ─────────────────────────────────────────────────────────
-const SUPABASE_URL         = 'https://TU_PROYECTO.supabase.co';
-const SUPABASE_KEY         = 'TU_ANON_PUBLIC_KEY';
-const SUPABASE_SERVICE_KEY = 'TU_SERVICE_ROLE_KEY'; // solo admin
+const SUPABASE_URL         = 'https://ygluxjlmazissytjdvze.supabase.co';
+const SUPABASE_KEY         = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbHV4amxtYXppc3N5dGpkdnplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTg2NzIsImV4cCI6MjA5NzM5NDY3Mn0.Q-7CDEPkimkSWuVC4WWzi79wwFWxxJkIOoYg4DaBnh0';
+const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbHV4amxtYXppc3N5dGpkdnplIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTgxODY3MiwiZXhwIjoyMDk3Mzk0NjcyfQ.bWySmfwIngwXB1yqpaYGYwvBeOyWDWeqjPzGBMEk_1Y'; // solo admin
